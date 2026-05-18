60449f21392985fc0a8e1bc5e402fa4e192f9db3
Marsouel Ngouadjo <marsouel.ngouadjio@saintjeaningenier.org>
2026-05-18 09:49:06 +0100

    creation de la page not found

--- frontend/GRH/src/app/app.routes.ts
+++ frontend/GRH/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : 'accuiel' , component : Login},
+    { path: '**', redirectTo: 'login' }
+];
---
+++ frontend/GRH/src/app/components/page-not-found/page-not-found.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+
+@Component({
+  selector: 'app-page-not-found',
+  imports: [RouterLink],
+  templateUrl: './page-not-found.html',
+  styleUrl: './page-not-found.css',
+})
+export class PageNotFound {
+
+}

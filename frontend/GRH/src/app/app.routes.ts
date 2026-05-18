ec9d4fd796d82890dbea88b7546b40587e00e6ef
Marsouel Ngouadjo <marsouel.ngouadjio@saintjeaningenier.org>
2026-05-18 10:58:09 +0100

    Fix: Remplacement du dossier Angular par la version stable de la branche frontend

--- frontend/GRH/src/app/app.routes.ts
+++ frontend/GRH/src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
-import { PageNotFound } from './components/page-not-found/page-not-found';
 
 export const routes: Routes = [
-    {path : 'login' , component : Login},
-    {path : 'page-not-found' , component : PageNotFound },
-    
+    {path : 'accuiel' , component : Login},
+    { path: '**', redirectTo: 'login' }
 ];
--- frontend/GRH/src/app/components/login/login.ts
+++ frontend/GRH/src/app/components/login/login.ts
@@ -1,32 +1,32 @@
-import { Component, inject } from '@angular/core'; 
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { NgIf } from '@angular/common';
+import { email } from '@angular/forms/signals';
 import { jwtDecode } from 'jwt-decode'; 
 
 interface CustomJwtPayload {
-  role?: string;      
+  role?: string;      // Le champ rôle envoyé par votre backend
   user_id?: number;
   exp?: number;
 }
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, NgIf],
+  imports: [ ReactiveFormsModule,NgIf],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  loginForm: FormGroup;
+   loginForm: FormGroup;
   errorMessage: string = '';
 
-  private fb = inject(FormBuilder);
-  private http = inject(HttpClient);
-  private router = inject(Router);
-
-  constructor() {
-    // Initialisation propre du formulaire
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]]
@@ -37,27 +37,30 @@ export class Login {
     if (this.loginForm.invalid) return;
 
     this.http.post<any>('http://localhost:8000/api/token/', this.loginForm.value).subscribe({
-      next: (response: any) => { 
-        // Sauvegarde du token d'accès pour l'intercepteur
+      next: (response) => {
+        //  Sauvegarde du token d'accès pour l'intercepteur
         localStorage.setItem('access_token', response.access);
         
         try {
-          // Décodage du token pour récupérer le vrai rôle de l'utilisateur
+          //  Décodage du token pour récupérer le vrai rôle de l'utilisateur
           const decoded = jwtDecode<CustomJwtPayload>(response.access);
           
+          //  Récupération du rôle (si absent du token, on applique 'EMPLOYE' par défaut)
           const userRole = decoded.role || 'EMPLOYE'; 
           
+        
           localStorage.setItem('user_role', userRole);
           
-          // Redirection
-          this.router.navigate(['/']);
+        
+          this.router.navigate(['/employes']);
           
         } catch (error) {
+         
           localStorage.setItem('user_role', 'EMPLOYE');
           this.router.navigate(['/employes']);
         }
       },
-      error: (err: any) => {
+      error: (err) => {
         this.errorMessage = "Email ou mot de passe incorrect.";
       }
     });

2ac6c7cf019e4d976e88ef77d042e82224557736
Marsouel Ngouadjo <marsouel.ngouadjio@saintjeaningenier.org>
2026-05-18 08:21:46 +0100

    ameloiration crud employees

--- backend/administrateur/serializers.py
+++ backend/administrateur/serializers.py
@@ -1,5 +1,19 @@
 from rest_framework import serializers
 from .models import Administrateur
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+       #  insère le rôle de l'employé directement dans le jeton JWT
+        token['role'] = user.role 
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 class AdministrateurSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False, style={'input_type': 'password'})
--- backend/administrateur/urls.py
+++ backend/administrateur/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import AdministrateurListCreateAPIView, AdministrateurDetailUodateDeleteAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from administrateur.serializers import MyTokenObtainPairView 
 
 urlpatterns = [
     path('administrateurs/', AdministrateurListCreateAPIView.as_view(), name='admin-list-create'),
     path('administrateurs/<int:pk>/', AdministrateurDetailUodateDeleteAPIView.as_view(), name='admin-detail'),
+    path('api/token/', MyTokenObtainPairView.as_path_view() if hasattr(MyTokenObtainPairView, 'as_path_view') else MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("login/", TokenObtainPairView.as_view(),name="Token_Obtain_Pair"),
     path("refresh/",TokenRefreshView.as_view(),name="Token_Refresh"),
 ]
